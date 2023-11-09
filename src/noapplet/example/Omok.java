da66a896edd33375d43af99893424381fea24238
SophiaMontenegro <sophiaazulmontenegro@gmail.com>
2023-11-08 21:34:50 -0700

    options shown and toolbar shown

--- src/noapplet/example/Omok.java
+++ src/noapplet/example/Omok.java
@@ -1,5 +1,7 @@
 package noapplet.example;
 
+import noapplet.NoApplet;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -7,40 +9,51 @@ import java.net.URL;
 
 //import static com.sun.java.swing.action.ActionUtilities.IMAGE_DIR;
 
-public class Omok {
+public class Omok extends NoApplet {
     //hover outline of where to place
+    /*
+    private final static Dimension DEFAULT_DIMENSION = new Dimension(400, 600);
+    public Omok(){
+        this(DEFAULT_DIMENSION);
+    }
+
+    public Omok(Dimension dim) {
+        super((JFrame) null, "Omok");
+        configureGUI();
+        setSize(dim);
+        //setResizable(false);
+        setLocationRelativeTo(null);
+    }*/
+
     public Omok() {
+
         var frame = new JFrame("Omok");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-
-        JToolBar toolBar = new JToolBar("Omok");
-        JButton play1 = new JButton(createImageIcon("play.png"));
-        final JLabel label = new JLabel();
-        play1.addActionListener(new ActionListener() {
-            //creates a new game
-            public void actionPerformed(ActionEvent e) {
-                int result = JOptionPane.showConfirmDialog(frame,"Sure? You want to exit?", "Swing Tester",
-                        JOptionPane.YES_NO_OPTION,
-                        JOptionPane.QUESTION_MESSAGE);
-                if(result == JOptionPane.YES_OPTION){
-                    label.setText("You selected: Yes");
-                }else if (result == JOptionPane.NO_OPTION){
-                    label.setText("You selected: No");
-                }else {
-                    label.setText("None selected");
-                }
-            }
-
-        });
-        play1.setToolTipText("Play a new game");
-        play1.setFocusPainted(false);
-        toolBar.add(play1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 
+        JToolBar toolBar = getToolBar(frame);
+        JPanel panel = getOptionPanel();
+        panel.add(toolBar);
 
-        var panel = new JPanel();
+        frame.add(panel);
+        //frame.add(toolBar, BorderLayout.NORTH);
+        //frame.setContentPane(panel);
+        //frame.pack();
+        frame.setVisible(true);
+        frame.setContentPane(panel);
+        frame.pack();
+
+        /*
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setVisible(true);*/
+
+    }
+
+    private static JPanel getOptionPanel() {
+        JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(300, 200));
         JRadioButton human = new JRadioButton("Human");
         JRadioButton computer = new JRadioButton("Computer");
@@ -53,7 +66,7 @@ public class Omok {
         panel.add(human);
         panel.add(computer);
         panel.add(play);
-        panel.add(label);
+        //panel.add(label);
 
         play.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
@@ -74,16 +87,37 @@ public class Omok {
             }
 
         });
-        frame.add(panel,BorderLayout.NORTH);
-        frame.add(toolBar, BorderLayout.NORTH);
-        /*
-        frame.setContentPane(panel);
-        frame.pack();
-        frame.setVisible(true);*/
-        frame.setVisible(true);
+        return panel;
     }
+
+    private JToolBar getToolBar(JFrame frame){
+        JToolBar toolBar = new JToolBar("Omok");
+        JButton play1 = new JButton(createImageIcon("play.png"));
+        final JLabel label = new JLabel();
+        play1.addActionListener(new ActionListener() {
+            //creates a new game
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog(frame,"You want to start a new Game?", "New Game",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE);
+                if(result == JOptionPane.YES_OPTION){
+                    label.setText("You selected: Yes");
+                }else if (result == JOptionPane.NO_OPTION){
+                    label.setText("You selected: No");
+                }else {
+                    label.setText("None selected");
+                }
+            }
+
+        });
+        play1.setToolTipText("Play a new game");
+        play1.setFocusPainted(false);
+        toolBar.add(play1);
+        return toolBar;
+    }
+
     private ImageIcon createImageIcon(String filename){
-        URL imageURL = getClass().getResource(filename);
+        URL imageURL = getClass().getResource("res"+filename);
         if(imageURL != null)
             return new ImageIcon(imageURL);
         return null;
@@ -91,6 +125,11 @@ public class Omok {
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> new Omok());
+        //new Omok(new String[]{"width=330", "height=350"}).run();
+        /*
+        Omok om = new Omok();
+        om.setVisible(true);
+        om.setDefaultCloseOperation(DISPOSE_ON_CLOSE);*/
     }
 
 }
