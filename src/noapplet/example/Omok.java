7108956a2dbd48f36643a34075431d82d73c7dff
SophiaMontenegro <sophiaazulmontenegro@gmail.com>
2023-11-08 23:10:20 -0700

    disable select buttons

--- src/noapplet/example/Omok.java
+++ src/noapplet/example/Omok.java
@@ -10,20 +10,9 @@ import java.net.URL;
 //import static com.sun.java.swing.action.ActionUtilities.IMAGE_DIR;
 
 public class Omok extends NoApplet {
-    //hover outline of where to place
-    /*
-    private final static Dimension DEFAULT_DIMENSION = new Dimension(400, 600);
-    public Omok(){
-        this(DEFAULT_DIMENSION);
-    }
-
-    public Omok(Dimension dim) {
-        super((JFrame) null, "Omok");
-        configureGUI();
-        setSize(dim);
-        //setResizable(false);
-        setLocationRelativeTo(null);
-    }*/
+    private static JRadioButton human;
+    private static JRadioButton computer;
+    private static JButton play;
 
     public Omok() {
 
@@ -35,38 +24,32 @@ public class Omok extends NoApplet {
 
         JToolBar toolBar = getToolBar(frame);
         JPanel panel = getOptionPanel();
-        panel.add(toolBar);
+        frame.add(toolBar, BorderLayout.NORTH);
 
-        frame.add(panel);
-        //frame.add(toolBar, BorderLayout.NORTH);
-        //frame.setContentPane(panel);
-        //frame.pack();
+        JMenuBar menuBar = getMenuBar(frame);
+        frame.setJMenuBar(menuBar);
+
+        frame.add(panel, BorderLayout.CENTER);
         frame.setVisible(true);
         frame.setContentPane(panel);
         frame.pack();
-
-        /*
-        frame.setContentPane(panel);
-        frame.pack();
-        frame.setVisible(true);*/
-
     }
 
     private static JPanel getOptionPanel() {
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(300, 200));
-        JRadioButton human = new JRadioButton("Human");
-        JRadioButton computer = new JRadioButton("Computer");
-        JButton play = new JButton("Play");
+        human = new JRadioButton("Human");
+        computer = new JRadioButton("Computer");
+        play = new JButton("Play");
         ButtonGroup group = new ButtonGroup();
-
+        group .add(computer);
+        group.add(human);
         JLabel select = new JLabel("Select opponent:");
 
         panel.add(select);
         panel.add(human);
         panel.add(computer);
         panel.add(play);
-        //panel.add(label);
 
         play.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
@@ -76,18 +59,77 @@ public class Omok extends NoApplet {
                 }
                 else if(human.isSelected()){
                     displayOp = "Human Selected!";
+                    setSelectButtonsEnable(false);
                 }
                 else if(computer.isSelected()){
                     displayOp = "Computer Selected!";
+                    setSelectButtonsEnable(false);
                 }
                 else{
-                    displayOp = "No Selection was made!";
+                    displayOp="No Selection!";
                 }
                 JOptionPane.showMessageDialog(panel, displayOp); // fancy dialog window
             }
 
         });
         return panel;
+    }
+
+    private static void setSelectButtonsEnable(boolean b) {
+        human.setEnabled(b);
+        computer.setEnabled(b);
+        play.setEnabled(b);
+    }
+
+    private JMenuBar getMenuBar(JFrame frame){
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu menu = new JMenu("Game");
+        menu.setMnemonic(KeyEvent.VK_G);
+        menu.getAccessibleContext().setAccessibleDescription("Game menu");
+        menuBar.add(menu);
+
+        JMenuItem newPlay = new JMenuItem("Play", KeyEvent.VK_P);
+        newPlay.setIcon(createImageIcon("play.png"));
+        newPlay.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_P, InputEvent.ALT_DOWN_MASK));
+        newPlay.getAccessibleContext().setAccessibleDescription(
+                "Play a new game");
+        newPlay.addActionListener(new ActionListener() {
+            //creates a new game
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog(frame,"You want to start a new Game?", "New Game",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE);
+                if(result == JOptionPane.YES_OPTION){
+                    //create new game
+                    JOptionPane.showMessageDialog(frame, "Here is your new game!");
+                    setSelectButtonsEnable(true);
+
+                }else if (result == JOptionPane.NO_OPTION) {
+                    //resume game
+                    //do nothing
+                }
+            }
+
+        });
+        menu.add(newPlay);
+        JMenuItem helpButton = new JMenuItem("Help", KeyEvent.VK_P);
+        helpButton.setIcon(createImageIcon("help.png"));
+        helpButton.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_Q, InputEvent.ALT_DOWN_MASK));
+        helpButton.getAccessibleContext().setAccessibleDescription(
+                "Play a new game");
+        helpButton.addActionListener(new ActionListener() {
+            //creates a new game
+            public void actionPerformed(ActionEvent e) {
+                //instructions
+                JOptionPane.showMessageDialog(frame, "The object of Omok is to get five consecutive stones.\nWhen its your turn try your best to win! \nHave fun!");
+            }
+
+        });
+        menu.add(helpButton);
+        return menuBar;
     }
 
     private JToolBar getToolBar(JFrame frame){
@@ -131,6 +173,7 @@ public class Omok extends NoApplet {
         exit.setFocusPainted(false);
         toolBar.add(exit);
         return toolBar;
+
     }
 
     private ImageIcon createImageIcon(String filename){
